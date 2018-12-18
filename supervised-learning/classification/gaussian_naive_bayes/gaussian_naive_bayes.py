864d4547166c464b00a9bf33ba4bfa389d3c61e2
mir1995 <mir00021@hotmail.com>
2018-12-18 22:51:50 +0100

    Changed script location

--- supervised-learning/classification/gaussian_naive_bayes/gaussian_naive_bayes.py
+++
@@ -1,6 +0,0 @@
-"""
-The following class fits a bayes model to data from 
-a pandas dataframe where the response is binary and 
-and the predictors are continuos assumed normally 
-distributed.
-"""
